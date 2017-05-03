264ab480e9c8b41edc2bdd9b0c7129170ddafc79
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-05-03 09:27:48 +0200

    WIP

--- src/controllers/hi.ts
+++ src/controllers/hi.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express';
+import * as Sequelize from 'sequelize';
 
-function sayHi (req: Request, res: Response) {
+function sayHi(req: Request, res: Response) {
     res.send({
         message: 'Hello, Halp team!'
     });
+    let sequelize = new Sequelize('postgres://root:postgres@localhost:5432/postgres');
+    sequelize
+        .authenticate()
+        .then(function (err) {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(function (err) {
+            console.log('Unable to connect to the database:', err);
+        });
 }
 
-export { sayHi }; 
+export { sayHi };
