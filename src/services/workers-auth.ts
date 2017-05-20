55683652b5091e9e29afbcdc7aa9ee8a3c8d52c2
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-05-20 16:18:43 +0200

    feat(auth): Add worker role in login request response

--- src/services/workers-auth.ts
+++ src/services/workers-auth.ts
@@ -6,3 +6,10 @@ export function getUserPasswordHash(username: string): Promise<string> {
             return data.password;
         });
 }
+
+export function getUserRole(username: string): Promise<string> {
+    return db.one('select e.role from employee e where e.username = ${username}', { username: username })
+        .then(data => {
+            return data.role;
+        })
+}
