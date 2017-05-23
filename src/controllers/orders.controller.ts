748f9963757c2d92d70f470831331a9a247057c6
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-05-23 10:50:15 +0200

    PR feedback

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -54,27 +54,26 @@ function getOrders(req: Request, res: Response, next: any) {
     });
 }
 
-
 function getUserOrders(req: Request, res: Response, next: any) {
   let decode = decodeToken(req.headers.token);
   let username = decode.sub;
-  db.any( "select b.title AS title, po.id, po.completed, p.name, p.lastname, p.room, po.orderDate, 'book' AS type\n" +
-                "from patientOrder po, patient p, bookloan bl, bookCopy bc, book b\n" +
-                "where po.idpatient = p.username and po.id = bl.id and bl.referenceNumber = bc.referenceNumber " +
-          "and bc.idBook = b.id and p.username = $1\n" +
-          "UNION ALL\n" +
-          "select i.name AS title, po.id, po.completed, p.name, p.lastname, p.room, po.orderDate, 'item' AS type\n" +
-                "from patientOrder po, patient p, purchase pu, itemCopy ic, item i\n" +
-                "where po.idpatient = p.username and po.id = pu.id and pu.referenceNumber = ic.referenceNumber " +
-          ' and ic.idItem = i.id and p.username = $1', username)
-  .then(data => {
-     res.status(200)
-            .json({
-              data: data
+  db.any("select b.title AS title, po.id, po.completed, p.name, p.lastname, p.room, po.orderDate, 'book' AS type\n" +
+    "from patientOrder po, patient p, bookloan bl, bookCopy bc, book b\n" +
+    "where po.idpatient = p.username and po.id = bl.id and bl.referenceNumber = bc.referenceNumber " +
+    "and bc.idBook = b.id and p.username = $1\n" +
+    "UNION ALL\n" +
+    "select i.name AS title, po.id, po.completed, p.name, p.lastname, p.room, po.orderDate, 'item' AS type\n" +
+    "from patientOrder po, patient p, purchase pu, itemCopy ic, item i\n" +
+    "where po.idpatient = p.username and po.id = pu.id and pu.referenceNumber = ic.referenceNumber " +
+    ' and ic.idItem = i.id and p.username = $1', username)
+    .then(data => {
+      res.status(200)
+        .json({
+          data: data
         });
-  }).catch(function (err){
-    return next(err);
-  });
+    }).catch(function (err) {
+      return next(err);
+    });
 }
 
 export { getOrders, changeStateOrder, deleteOrder, getUserOrders };
